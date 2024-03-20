5303975df408d9443ddbae5b202a94baa8e70efc
goronc <108907603+goronc@users.noreply.github.com>
2024-03-20 17:12:42 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.getElementById("recupererButton").addEventListener("click", function() {
-    var texte = document.getElementById("textInput").value;
-    document.getElementById("textRecupere").innerText = texte;
-    document.getElementById("myModal").style.display = "block";
+    displayNextLine();
+});
+
+document.getElementById("textInput").addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) { // key code 13 = entrée
+        event.preventDefault(); // Empêche le comportement par défaut de la touche "Entrée"
+        displayNextLine();
+    }
 });
 
 document.getElementsByClassName("close")[0].addEventListener("click", function() {
@@ -14,3 +19,17 @@ window.addEventListener("click", function(event) {
         modal.style.display = "none";
     }
 });
+
+function displayNextLine() {
+    var texte = document.getElementById("textInput").value;
+    var textLines = texte.split('\n');
+    var lastLineIndex = parseInt(document.getElementById("textInput").getAttribute("data-last-line")) || 0;
+    if (lastLineIndex < textLines.length) {
+        var nextLine = textLines[lastLineIndex++];
+        document.getElementById("textInput").setAttribute("data-last-line", lastLineIndex);
+        document.getElementById("textRecupere").innerText += nextLine + "\n";
+    } else {
+        document.getElementById("myModal").style.display = "none";
+    }
+    document.getElementById("myModal").style.display = "block";
+}
